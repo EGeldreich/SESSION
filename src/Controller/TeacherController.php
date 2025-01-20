6617f43d08722dd4b1ab2b30e99af4f3914439e3
Emmanuel Geldreich <egeldreich@gmail.com>
2025-01-20 09:53:46 +0100

    Created new page : show_teacher to see teachers details. Displayed informations on that page.

--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Teacher;
 use App\Repository\TeacherRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -17,6 +18,15 @@ final class TeacherController extends AbstractController
         return $this->render('teacher/index.html.twig', [
             'controller_name' => 'TeacherController',
             'teachers' => $teachers,
+        ]);
+    }
+    
+    #[Route('/teacher/{id}', name: 'show_teacher')]
+    public function show(Teacher $teacher): Response
+    {
+        return $this->render('teacher/show.html.twig', [
+            'controller_name' => 'teacherController',
+            'teacher' => $teacher,
         ]);
     }
 }
