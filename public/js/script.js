295c2b0956fa625ecf59d0922f35bcb0cce0d33f
Emmanuel G <stdgeld@gmail.com>
2025-01-22 18:58:14 +0100

    Placed flash messages in base.html.twig. Styled them, and created a script to fade them out after a few seconds.

--- public/js/script.js
+++ public/js/script.js
@@ -89,3 +89,21 @@ document.addEventListener('turbo:load', function() {
     }
   }
 });
+
+// FLASH MESSAGES DISMISS ______________________________________________________________
+document.addEventListener('turbo:load', function() {
+  let flashMessage = document.querySelector('.flash');
+
+  if (flashMessage) {
+    // Wait 2 seconds before fading out
+    setTimeout(() => {
+      flashMessage.style.transition = 'opacity 1s';
+      flashMessage.style.opacity = '0';
+      // After that, wait 100 miliseconds before removing element
+      setTimeout(() => {
+        flashMessage.style.display = 'none';
+      }, 100);
+
+    }, 2000);
+  }
+});
