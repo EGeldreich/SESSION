1e63f949e6c19992da73cb67ed2687be0da14761
Emmanuel G <stdgeld@gmail.com>
2025-01-22 15:08:36 +0100

    Finished styling Show Session view. Still need to add modals to be able to add students and lessons. Added turbo:load eventlistener so JS works correctly

--- public/js/script.js
+++ public/js/script.js
@@ -22,25 +22,28 @@
 // });
 
 // SESSION LIST DISPLAY _________________________________________________________
-let sessionBtn = document.querySelectorAll('.session-display-btn');
-let sessionLists = document.querySelectorAll('.session-card-container');
+document.addEventListener('turbo:load', function() {
+  let sessionBtn = document.querySelectorAll('.session-display-btn');
+  let sessionLists = document.querySelectorAll('.session-card-container');
 
-sessionBtn.forEach(btn => {
-  btn.addEventListener('click', function() {
-    let target = btn.id.replace('-btn', '');
-    let targetList = document.querySelector(`.session-card-container__${target}`);
+  sessionBtn.forEach(btn => {
+    btn.addEventListener('click', function() {
+      let target = btn.id.replace('-btn', '');
+      let targetList = document.querySelector(`.session-card-container__${target}`);
 
-    sessionBtn.forEach(btn => {
-      btn.classList.remove('blue-bg');
-      btn.classList.add('grey2-bg');
+      sessionBtn.forEach(btn => {
+        btn.classList.remove('blue-bg');
+        btn.classList.add('grey2-bg');
+      });
+      sessionLists.forEach(list => list.classList.add('hidden'));
+      targetList.classList.remove('hidden');
+
+      btn.classList.remove('grey2-bg');
+      btn.classList.add('blue-bg');
     });
-    sessionLists.forEach(list => list.classList.add('hidden'));
-    targetList.classList.remove('hidden');
-
-    btn.classList.remove('grey2-bg');
-    btn.classList.add('blue-bg');
   });
 });
+
 // MODAL HANDLING ______________________________________________________________
 // Get the modal
 let modal = document.querySelector(".modal");
